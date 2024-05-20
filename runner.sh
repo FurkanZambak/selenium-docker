87ca207ddf26081c49fa9a5f31e6dfd790ec2985
furkan.zambak <furkan.zambak@blackkite.com>
2024-05-20 17:05:01 +0300

    Runner.sh

--- runner.sh
+++ runner.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------
-#  This script expects the following environment variables
+#  This script expects the following environment variables:
 #     HUB_HOST
 #     BROWSER
 #     THREAD_COUNT
 #     TEST_SUITE
 #-------------------------------------------------------------------
 
-# Let's print what we have received
+# Print the received environment variables
 echo "-------------------------------------------"
 echo "HUB_HOST      : ${HUB_HOST:-hub}"
 echo "BROWSER       : ${BROWSER:-chrome}"
@@ -16,25 +16,23 @@ echo "THREAD_COUNT  : ${THREAD_COUNT:-1}"
 echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 
-# Do not start the tests immediately. Hub has to be ready with browser nodes
-echo "Checking if hub is ready..!"
+# Check if the hub is ready
+echo "Checking if the hub is ready..."
 count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
+while [ "$(curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready)" != "true" ]; do
   count=$((count+1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ];
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
+  if [ "$count" -ge 30 ]; then
+    echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+    exit 1
   fi
   sleep 1
 done
 
-# At this point, selenium grid should be up!
+# Selenium Grid is up! Run the test
 echo "Selenium Grid is up and running. Running the test...."
 
-# Start the java command
+# Start the Java command
 java -cp 'libs/*' \
      -Dselenium.grid.enabled=true \
      -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
